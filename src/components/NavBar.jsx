5a21d397de2bd6fdbeff143ef3c9c23b0e719514
apit putra <apiiettrecomen1@gmail.com>
2024-03-19 17:00:54 +0700

    refactor: fixed navbar and start page to top

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,42 @@
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Button from "./Button";
-import LinkRoute from "./LinkRoute";
 import Logo from "./Logo";
 import OutlinedButton from "./OutlinedButton";
 import NavLinkRoute from "./NavLinkRoute";
+import { useEffect, useState } from "react";
 
 function NavBar() {
   const navigate = useNavigate();
   const style =
     "text-white hover:text-slate-200 w-fit transition duration-300 text-lg";
+  const [isScrolled, setIsScrolled] = useState(false);
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: "smooth",
+    });
+  }, [location.pathname]);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollTop =
+        window.pageYOffset || document.documentElement.scrollTop;
+      setIsScrolled(scrollTop > 0);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
 
   return (
-    <nav className="flex items-center justify-between px-12 py-5 bg-transparent absolute  w-full z-10">
+    <nav
+      className={`flex items-center justify-between px-12 py-5 fixed w-full z-10 ${
+        isScrolled ? "bg-color-secondary" : "bg-transparent"
+      }`}
+    >
       <Logo />
       <ul className="flex gap-5">
         <li>
